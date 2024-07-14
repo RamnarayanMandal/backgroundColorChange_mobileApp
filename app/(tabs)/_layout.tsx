19b7520df3205c09c46183fc33890fa47d96fbe4
RamnarayanMandal <ramnarayan847231@gmail.com>
2024-07-14 23:49:02 +0530

    add files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,58 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import React, { useState } from 'react'
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const [backgroundColor, setBackground] =useState('white')
 
+  const handleOnPress = () => {
+    const hexRange = "0123456789ABCDEF"
+    let color = "#" 
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    
+    setBackground(color)
+
+  }
+
+//  set background
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <>
+      <StatusBar backgroundColor={"black"} />
+      <View style={[style.container,{backgroundColor:backgroundColor}]}>
+        <TouchableOpacity>
+         <View style={style.actionBtn}>
+         <Text style={style.actionBtnText} onPress={handleOnPress}>
+            press me
+          </Text>
+         </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionBtn: {
+    paddingVertical: 10,
+    paddingHorizontal: 14,
+    backgroundColor: '#6A1B4D',
+    borderRadius: 8,
+    elevation: 5,
+
+
+  },
+  actionBtnText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+
+})
